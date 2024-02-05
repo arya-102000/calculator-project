4421fb6af6c2e0ba18cccd17edcf5aa65ec5e940
arya-102000 <aryavs819@gmail.com>
2024-02-05 11:35:33 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function addToDisplay(value){
+    document.getElementById('pre').value += value;
+  }
+  function clearDisplay(){
+    document.getElementById('pre').value ='';
+  }
+  function calculate(){
+    try{
+      var result = eval(document.getElementById('pre').value);
+      document.getElementById('pre').value = result;
+    }catch(error){
+      document.getElementById('pre').value ='Error';
+    }
+  }
+  function clearLast(){
+    var display = document.getElementById('pre');
+    display.value = display.value.slice(0,-1);
+  }
